815f954ae7f673ba864aa19b2d2fbd6ebd79c380
BATIKAN BORA ORMANCI <57166099+batikanor@users.noreply.github.com>
2025-02-10 11:26:49 +0100

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,15 +4,16 @@ const nextConfig = {
     output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
   
     // Set base path only for GitHub Pages, leave empty if using a custom domain
-    basePath: process.env.NODE_ENV === 'production' ? '/nextathon-template' : '',
-  
+    // basePath: process.env.NODE_ENV === 'production' ? '/nextathon-template' : '',
+    basePath: process.env.NODE_ENV === 'production' ? "" : "",
+
     // Disable server-based image optimization for static export compatibility
     images: {
       unoptimized: true,
     },
   
     // Set assetPrefix for production if assets are hosted separately
-    assetPrefix: process.env.NODE_ENV === 'production' ? 'https://www.yourdomain.com/' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? 'https://www.batikanor.com/nextathon-template' : '',
   
     // Enable trailing slashes in URLs for correct resolution
     trailingSlash: true,
